caab1a9156e2515c0249533e780131a603632156
0nly420 <131375398+0nly420@users.noreply.github.com>
2024-04-03 22:22:20 -0500

    Initial commit

---
+++ Ravn-Challenge-KevinCuchon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ravn-Challenge-KevinCuchon
+//
+//  Created by Kevin Cuchon Morales on 3/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ravn-Challenge-KevinCuchon/Ravn_Challenge_KevinCuchonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ravn_Challenge_KevinCuchonApp.swift
+//  Ravn-Challenge-KevinCuchon
+//
+//  Created by Kevin Cuchon Morales on 3/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Ravn_Challenge_KevinCuchonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
